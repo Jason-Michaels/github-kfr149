5eecbf50f84f5daea44312f22839c4dd33966cfb
Jason-Michaels <jason.michaels721@gmail.com>
2019-07-04 23:04:57 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as myconfig from './test.json';
 
 @Component({
   selector: 'app-root',
@@ -22,4 +23,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quickProfile';
+
+  constructor() {
+    console.log(myconfig)
+  }
 }
